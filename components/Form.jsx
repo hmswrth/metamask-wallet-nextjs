a55dbf1f6fde69fd0dc802fb85a7d220980487c2
Hemanth Mudra <mudra.hemanth@gmail.com>
2022-01-10 04:15:59 +0530

    added form

---
+++ components/Form.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import formStyle from '../styles/Form.module.css';
+import { FcRefresh } from 'react-icons/fc'
+import { IconContext } from "react-icons";
+
+function Form() {
+
+   const [nep, setNep] = useState('')
+   const [busd, setBusd] = useState('')
+
+   const handleNepChange = (e) => {
+      e.preventDefault();
+      setNep(() => e.target.value)
+      setBusd(() => {
+         if (e.target.value != 0.00 || e.target.value != '')
+            return ((parseFloat(e.target.value) * 3).toFixed(2)).toString();
+         return '';
+      })
+   }
+
+   const handleBusdChange = (e) => {
+      e.preventDefault();
+      setBusd(() => e.target.value)
+      setNep(() => {
+         if (e.target.value != 0.00 || e.target.value != '')
+            return ((parseFloat(e.target.value) / 3).toFixed(2)).toString();
+         return '';
+      })
+   }
+
+   return (
+      <div className={formStyle.container}>
+         <div className={formStyle.card}>
+            <form >
+               <div><h3 className={formStyle.cardTitle}>Crypto converter</h3></div>
+               <div className={formStyle.cardItem}>
+                  <label >NEP</label> <br />
+                  <input type='text'
+                     id='nep'
+                     placeholder='0.00'
+                     onChange={handleNepChange}
+                     value={nep}
+                  />
+               </div>
+               {/* <div className={formStyle.cardItem}><FcRefresh /></div> */}
+               {/* <IconContext.Provider value={{ className: 'react-icons', size: '1.5rem' }}>
+                  <div className={formStyle.icon}>
+                     <FcRefresh />
+                  </div>
+               </IconContext.Provider> */}
+               <div className={formStyle.cardItem}>
+                  <label >BUSD</label> <br />
+                  <input type='text'
+                     id='busd'
+                     placeholder='0.00'
+                     onChange={handleBusdChange}
+                     value={busd} />
+               </div>
+               <div>
+                  <a >check wallet details</a>
+               </div>
+            </form>
+         </div>
+
+      </div >
+   )
+}
+
+export default Form
